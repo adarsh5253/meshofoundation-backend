a8a86c7fb3da336b31e8038eeb7052ef87789531
Adarsh Tiwari <adarshtiwari5122002@gmail.com>
2026-05-21 09:58:12 +0530

    Updated CORS settings

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const app = express();
 // --- CORS ---
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://meshofoundation-frontend.vercel.app",
+  "https://ngowebsite-blush.vercel.app/",
   // https://meshofoundation-frontend.vercel.app/
 ];
 
